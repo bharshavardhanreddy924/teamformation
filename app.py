0dc56336b226b5d64112c11b026c8085037dfaf8
Harshavardhan Reddy <151720877+bharshavardhanreddy924@users.noreply.github.com>
2025-03-28 21:57:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -834,6 +834,7 @@ def process_invitation():
         )
     
     return redirect(url_for('dashboard'))
-
-if __name__ == '__main__':
-    app.run(debug=True) 
+    
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
